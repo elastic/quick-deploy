4f9f3feb56eb6959e1b7906072d59fcaa7eea59a
Ethan Groves <ethan.groves@gmail.com>
2018-10-03 08:08:20 -0400

    automate add registry

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 sudo apt-get update && sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update && sudo apt-get install ansible -y
 
 
