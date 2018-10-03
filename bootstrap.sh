d64333347f255f512660111e31fe75b873e6d930
Ethan Groves <ethan.groves@gmail.com>
2018-10-03 08:00:35 -0400

    Updated ansible user variable in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,4 @@ sudo apt-get update && sudo apt-get install ansible -y
 export DOCKER_COMPOSE_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | awk -F / '{print $NF}')
 export CURRENT_USER=$(whoami)
 
-ansible-playbook ./bootstrap.yml -e docker_compose_version=$DOCKER_COMPOSE_VERSION -e current_user=$CURRENT_USER
+ansible-playbook ./bootstrap.yml -e docker_compose_version=$DOCKER_COMPOSE_VERSION -e user=$CURRENT_USER
