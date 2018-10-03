9c8fb9cc0c86963e8de5abf06314318188c94e6f
Ethan Groves <ethan.groves@gmail.com>
2018-10-02 20:36:57 -0400

    Added -y flag to install commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-sudo apt-get update && sudo apt-get install software-properties-common
+sudo apt-get update && sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update && sudo apt-get install ansible
+sudo apt-get update && sudo apt-get install ansible -y
 
 ansible-playbook ./bootstrap.yml
