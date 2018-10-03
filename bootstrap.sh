d132777d15b31c79c51067a6041bb6eda52529bd
Ethan Groves <ethan.groves@gmail.com>
2018-10-03 07:58:54 -0400

    Updated docker-compose install

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,8 @@ sudo apt-get update && sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update && sudo apt-get install ansible -y
 
-ansible-playbook ./bootstrap.yml
+
+export DOCKER_COMPOSE_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | awk -F / '{print $NF}')
+export CURRENT_USER=$(whoami)
+
+ansible-playbook ./bootstrap.yml -e docker_compose_version=$DOCKER_COMPOSE_VERSION -e current_user=$CURRENT_USER
